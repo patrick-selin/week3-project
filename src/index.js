64934b5014b8f60e2f506a9a308c2e3cc0b0cd38
Patrick Selin <patrick.selin@gmail.com>
2023-06-08 08:50:00 +0300

    3

--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ const btnSubmit = document.getElementById("submit-data");
 const btnErase = document.getElementById("empty-table");
 
 const inputUserName = document.getElementById("input-username");
-const inputEmail= document.getElementById("input-email");
+const inputEmail = document.getElementById("input-email");
 const inputAddress = document.getElementById("input-address");
 const inputIsAdmin = document.getElementById("input-admin");
 
 const tableBody = document.querySelector('#table tbody');
 
-btnSubmit.addEventListener('click', () => {
-    event.preventDefault();
-    
-    const userName = inputUserName.value;
-    const email = inputEmail.value;
-    const address = inputAddress.value;
-    const isAdmin = inputIsAdmin.checked;
 
+const userNameList = document.querySelector('.user-name');
+
+
+btnSubmit.addEventListener('click', () => {
+  event.preventDefault();
+
+  const userName = inputUserName.value;
+  const email = inputEmail.value;
+  const address = inputAddress.value;
+  const isAdmin = inputIsAdmin.checked;
+
+  const existingRow = Array.from(tableBody.rows).find(row =>
+    row.cells[0].textContent === userName);
+
+  if (existingRow) {
+    existingRow.cells[1].textContent = email;
+    existingRow.cells[2].textContent = address;
+    existingRow.cells[3].textContent = isAdminChecked(isAdmin);
+
+  } else {
     const newRow = tableBody.insertRow();
 
     const newCell1 = newRow.insertCell();
@@ -28,20 +41,26 @@ btnSubmit.addEventListener('click', () => {
     newCell3.textContent = address;
 
     const newCell4 = newRow.insertCell();
+    newCell4.textContent = isAdminChecked(isAdmin);
+  }
+});
 
-    if (isAdmin) {
-        newCell4.textContent = "X";
-    } else {
-        newCell4.textContent = "-";
-    }
-  });
 
-  
-  btnErase.addEventListener('click', () => {
-    event.preventDefault();
-    
-    while (tableBody.firstChild) {
-      tableBody.removeChild(tableBody.firstChild);
-    }
-  });
+btnErase.addEventListener('click', () => {
+  event.preventDefault();
+
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+});
+
+
+function isAdminChecked(isAdmin) {
+  if (isAdmin) {
+    return "X";
+  } else {
+    return "-";
+  }
+}
+
 
