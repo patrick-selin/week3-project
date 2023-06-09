efffa08587b2ba952451fda273fc28186bc40c65
Patrick Selin <patrick.selin@gmail.com>
2023-06-09 11:10:09 +0300

    csw

--- src/index.js
+++ src/index.js
@@ -9,35 +9,28 @@ fetch('./001_11s5_2022m12.json')
         const populations = data.dataset.value;
 
         console.log(municipalities);
-        console.log(populations);
 
         console.log(typeof municipalities)
       
-        console.log(municipalities.length)
         
         for (let i = 0; i < Object.keys(municipalities).length; i++) {
-            console.log("no")
-            const municipality = municipalities[i];
-            // console.log(municipality);
-            console.log(Object.keys(municipalities));
-
-
+            console.log(municipalities);
+            const municipality = municipalities[i];        
             const population = populations[i];
+
             const row = document.createElement('tr');
 
             const municipalityCell = document.createElement('td');
             municipalityCell.textContent = municipality;
             row.appendChild(municipalityCell);
-            console.log("no")
-
+            
             const populationCell = document.createElement('td');
             populationCell.textContent = population;
             row.appendChild(populationCell);
 
             tbody.appendChild(row);
         }
-        console.log("no")
-        console.log(Object.keys(municipalities));
+        
     });
 
 
