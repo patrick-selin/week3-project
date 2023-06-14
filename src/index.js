ce1fa7d223cb1ba51934f7fcde0b22b78958cb8b
Patrick Selin <patrick.selin@gmail.com>
2023-06-14 10:35:55 +0300

    cleaned the code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-let emp;
+let employer;
 
 fetch('https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065 ')
     .then((response) => {
         return response.json();
     })
     .then((data2) => {
-        // const tbody = document.getElementById('data-output');
-        emp = data2.dataset.value;
-
-        
+        employer = data2.dataset.value;
     });
 
 
@@ -18,16 +15,13 @@ fetch('https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff ')
     })
     .then((data) => {
         const tbody = document.getElementById('data-output');
-
         const municipalities = data.dataset.dimension.Alue.category.label;
         const populations = data.dataset.value;
         
-      
         for (let i = 0; i < Object.keys(municipalities).length; i++) {
             
             const municipality = Object.values(municipalities)[i];    
             const population = populations[i];
-         
             const row = document.createElement('tr');
 
             const municipalityCell = document.createElement('td');
@@ -39,20 +33,18 @@ fetch('https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff ')
             row.appendChild(populationCell);
 
             const employmentCell = document.createElement('td');
-            employmentCell.textContent = emp[i];
+            employmentCell.textContent = employer[i];
             row.appendChild(employmentCell);
 
             const empPerCell = document.createElement('td');
-            let empPer = (emp[i] / populations[i]) * 100;
+            let empPer = (employer[i] / populations[i]) * 100;
             empPerCell.textContent = empPer.toFixed(2) + "%";
 
             if (empPer > 45) {
-                console.log("yli");
                 row.style.backgroundColor = "#abffbd";
             }
 
             if (empPer < 25) {
-                console.log("yli");
                 row.style.backgroundColor = "#ff9e9e";
             }
             
